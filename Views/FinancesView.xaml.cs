0d69df80d13b4c0fb212a86734acb7d2ff4498bb
Muwonge Hassan Saava <hassansaava@gmail.com>
2023-12-12 18:13:00 +0300

    added content control, and home view

---
+++ Views/FinancesView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace LoginForm.Views
+{
+    public partial class FinancesView : UserControl
+    {
+        public FinancesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
